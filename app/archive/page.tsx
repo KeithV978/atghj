bb1c380c4b3afb18ee7a1ce9e76d405fa87c0870
victor olorunda <victorolorunda6@gmail.com>
2025-11-03 23:00:19 +0100

    renewed user interface

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useState } from 'react';
 import ArchiveFilter from '@/app/components/archive/ArchiveFilter';
 import ArchiveGrid from '@/app/components/archive/ArchiveGrid';
